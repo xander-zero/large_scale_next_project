ccd0bfcdb7acde01aa9fb63f7fcd77ddf96e9881
xander-zero <mahmud.ansari60636@gmail.com>
2024-10-06 11:16:20 +0330

    feat: adding test snapshot for home page section

---
+++ src/app/_components/HomeHeroSection/home-hero-section.tsx
@@ -0,0 +1,48 @@
+import Image from 'next/image';
+import { Button } from '../button';
+
+const HomeHeroSection: React.FC = () => {
+  return (
+    <section className="bg-hero-pattern bg-no-repeat bg-center mt-5 xl:mt-20 xl:bg-left">
+      <div className="container flex flex-col-reverse items-center xl:flex-row">
+        <div className="flex flex-col gap-5 mt-12 pb-5 text-center xl:text-right">
+          <h3 className="text-cl dark:text-info xl:text-2xl">
+            خوش آمدید به ...
+          </h3>
+          <h1 className="text-3xl font-black gradiant lg:text-3xl xl:text-5xl">
+            مسیر صعود به قله های برنامه نوسی
+          </h1>
+          <p>
+            ری‌اکت و نکست‌جی‌اس برترین کتابخونه‌های فرانت‌اند و یکه‌تاز دنیای
+            وب! پیشرفته‌ترین مباحث رو اینجا می تونی پیدا کنی. پس همین الان
+            یادگیری رو شروع کن ما هم از ابتدای مسیر با آموزش‌های تخصصی و کاملاً
+            کاربردی کنارت هستیم.
+          </p>
+          <div className="my-5 flex gap-4">
+            <Button size="large" variant="primary">
+              دوره های ریکت و نکست
+            </Button>
+            <Button size="large" variant="neutral">
+              مشاوره برنامه نویسی
+            </Button>
+          </div>
+          <Image
+            src="/images/frameworks.png"
+            alt=""
+            width={412}
+            height={39}
+            className="grayscale mt-4 opacity-70 m-auto xl:m-0"
+          />
+        </div>
+        <Image
+          src="/images/programmer-landing.svg"
+          width={702}
+          height={521}
+          alt="xander"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default HomeHeroSection;
